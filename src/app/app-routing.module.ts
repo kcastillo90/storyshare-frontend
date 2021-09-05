fc5d70e62c532ecd85b059c5545f06c7042dc5f8
Kyle Castillo <kyle.castillo90@gmail.com>
2021-09-05 19:14:47 -0400

    added auth0 for sessions

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { AllStoriesComponent } from './all-stories/all-stories.component'
 import { SingleStoryComponent } from './single-story/single-story.component'
 import { StoryFormComponent } from './story-form/story-form.component'
+import { Auth0LoginComponent } from './auth0-login/auth0-login.component'
 
 
 const routes: Routes = [
